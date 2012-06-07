52b07795c7a61d52f8a4d79e2a46571098fa5113
Riccardo Degan <riccardo.degan@diginess.it>
2012-06-07 14:18:08 +0200

    Fix for spree 1.1-stable

--- app/controllers/spree/admin/menus_controller.rb
+++ app/controllers/spree/admin/menus_controller.rb
@@ -1,5 +1,5 @@
 module Spree
-    module Admin
+  module Admin
 
 
     class MenusController < ResourceController
@@ -9,12 +9,12 @@ module Spree
         params[:search] ||= {}
         params[:search][:meta_sort] ||= "name.asc"
 
-        @menus = @search.page(params[:page]).per(Spree::Config[:admin_products_per_page])
+        @menus = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
         #render :layout => 'admin'
       end
 
       def collection
-        @search = super.metasearch(params[:search])
+        @search = super.ransack(params[:search])
       end
 
       def new
@@ -28,16 +28,64 @@ module Spree
 
 
       def update_positions
-              params[:positions].each do |id, index|
-                Spree::Menu.update_all(['position=?', index], ['id=?', id])
+        params[:positions].each do |id, index|
+          Spree::Menu.update_all(['position=?', index], ['id=?', id])
+        end
+
+        respond_to do |format|
+          format.html { redirect_to admin_menus_url() }
+          format.js  { render :text => 'Ok' }
+        end
+      end
+      
+=begin
+        TODO da togliere: sarebbe preferibile lasciare :
+             def location_after_save
+                 admin_static_pages_url
+             end
+        e togliere:
+          - def update
+          - def create
+=end
+
+              def update
+                invoke_callbacks(:update, :before)
+                if @object.update_attributes(params[object_name])
+                  invoke_callbacks(:update, :after)
+                  flash.notice = flash_message_for(@object, :successfully_updated)
+                  redirect_to location_after_save
+                  #respond_with(@object) do |format|
+                  #  format.html { redirect_to self.location_after_save }
+                  #  format.js   { render :layout => false }
+                  #end
+                else
+                  invoke_callbacks(:update, :fails)
+                  respond_with(@object)
+                end
               end
 
-              respond_to do |format|
-                format.html { redirect_to admin_menus_url() }
-                format.js  { render :text => 'Ok' }
+              def create
+                invoke_callbacks(:create, :before)
+                if @object.save
+                  invoke_callbacks(:create, :after)
+                  flash.notice = flash_message_for(@object, :successfully_created)
+                  redirect_to location_after_save
+                  #respond_with(@object) do |format|
+                  #  format.html { redirect_to location_after_save }
+                  #  format.js   { render :layout => false }
+                  #end
+                else
+                  invoke_callbacks(:create, :fails)
+                  respond_with(@object)
+                end
               end
-            end
 
+
+            protected
+
+              def location_after_save
+                 admin_menus_url
+              end
 
     end
   end
