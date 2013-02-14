8e08e4b67e5e213e7df7d14b283789b1b40fb67f
Damiano <damiano.giacomello@diginess.it>
2013-02-14 11:32:02 +0100

    fixed locales and presentation.

--- app/models/spree/menu.rb
+++ app/models/spree/menu.rb
@@ -2,7 +2,10 @@ module Spree
   class Menu < ActiveRecord::Base
     belongs_to :linkable, :polymorphic => true
     belongs_to :parent, :foreign_key => :parent_id, :class_name => 'Spree::Menu'
-    has_many :children, :class_name => 'Spree::Menu', :foreign_key => :parent_id
+    has_many :children, :class_name => 'Spree::Menu', :foreign_key => :parent_id, :dependent => :destroy
+    
+    before_create :set_presentation
+    before_update :set_presentation
     
     attr_accessible :presentation, :url, :parent, :position, :visible, :name, :linkable, :parent_id,
                     :linkable_id, :linkable_type, :category, :class_list
@@ -22,6 +25,10 @@ module Spree
         last_menu = Menu.where({:category => (self.category rescue ""), :parent_id => (self.parent_id rescue nil)}).last
         self.position = last_menu ? last_menu.position + 1 : 0
       end
+    end
+    
+    def set_presentation
+      self.presentation = self.name.titleize
     end
   end
 end
